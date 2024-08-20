f24d924a73488cf2607bb9c99b537243740dc650
Deepak <Deepak@dpcodes>
2024-08-20 21:07:08 +0530

    learned JS Variables

---
+++ index.js
@@ -0,0 +1,42 @@
+//Variables
+//var let const
+
+//functional scoped
+// console.log(userName2)
+var userName1 = "Hello All"
+
+// {
+//     var userName1 = "Vanakam"
+//     console.log(userName1)
+// }
+
+getName()
+
+function getName() {
+    var userName1 = "Abdul"
+    console.log(userName1)
+}
+
+// getName()
+
+// console.log(userName1)
+
+
+let userName2;
+userName2 = "hey"
+
+// {
+//    let userName2 = "Deepak"
+//    console.log(userName2)
+// }
+
+// console.log(userName2)
+
+// const userName3 = "VasuDevan"
+// {
+//     const userName3 = "Vinoth"
+//     console.log(userName3)
+// }
+// console.log(userName3)
+
+// const,let-block scoped
