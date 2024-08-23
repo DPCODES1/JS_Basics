d2876a151f8878f5fe1625813cc7cc4ec2ec7c08
Deepak <Deepak@dpcodes>
2024-08-23 20:58:37 +0530

    operators finished

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-//data-types
-//string numbers arrays objects bigInt booleans
-//undefined null NaN Infinity
-
-console.log(typeof("Hello Abdul"))
-console.log(typeof(23.29))
-           //0 1 2 3 4 ....
-const arr = [1,2,3,"Hello",[1,2,3]]
-console.log(arr)
-
-const car = {0:"MRF",engine:"V8",tyres:"CEAT"}
-console.log(car[0])
-
-
-let num1 = 2
-const num2 = num1
-num1 = 3
-console.log(num1,num2)
-
-// const arr2 = arr
-// arr[0] = "Vanakam"
-// console.log(arr,arr2)
-
-// const car1 = car
-// car.engine = "V3"
-// console.log(car1,car)
-
-// console.log(true || false)
-
-// console.log(Boolean(-1))
-
 //operators
 //arithmetic #assignment #comparitive #logical #ternary #null-coelizing
+
+//Arithmetic
+// + - * / ** %
+console.log(3**3)
+
+//Assignment
+// = += -= *= /= **= %=
+let a = 2
+let b = 2
+a**=b //a=a**b
+console.log(a)
+
+//comparitive
+//== === < > <= >= != !==
+// console.log(2 == '2') //== it only checks value
+// console.log(2==="2") //=== it checks type as well as value
+// console.log(2!=="2") 
+
+//logical
+// && || !
+
+// console.log(!(2==2 || 3==2))
+
+//ternary ?:
+// cond ? true_val : false_val
+
+// console.log(2==1 ? 2 : 1)
+
+//null-coelizing
+// console.log("" ?? 2)
+
