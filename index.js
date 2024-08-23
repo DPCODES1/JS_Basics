b46f07a8bd405492fec74c5ca1848b2c6271fcb5
Deepak <Deepak@dpcodes>
2024-08-23 21:07:17 +0530

    added function intro

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-//operators
-//arithmetic #assignment #comparitive #logical #ternary #null-coelizing
+add(2,3)
 
-//Arithmetic
-// + - * / ** %
-console.log(3**3)
+function add(a,b) {
+    console.log(a+b)
+}
 
-//Assignment
-// = += -= *= /= **= %=
-let a = 2
-let b = 2
-a**=b //a=a**b
-console.log(a)
+//IIFE
+(function (a,b) {console.log(a,b)})(2,3)
 
-//comparitive
-//== === < > <= >= != !==
-// console.log(2 == '2') //== it only checks value
-// console.log(2==="2") //=== it checks type as well as value
-// console.log(2!=="2") 
-
-//logical
-// && || !
-
-// console.log(!(2==2 || 3==2))
-
-//ternary ?:
-// cond ? true_val : false_val
-
-// console.log(2==1 ? 2 : 1)
-
-//null-coelizing
-// console.log("" ?? 2)
-
+add(5,2)
